700f88fa8d95bb92d78a3b9c3bd07789300a197e
anushqakamble-sketch <anushqakamble@gmail.com>
2026-06-27 18:54:35 -0400

    Update ddl_gold.sql

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -11,14 +11,8 @@ Script purpose:
 Usage: These views can be queried directly for analytics and reporting.
 */
 =========================================================================================================
--- Creating an alias 
--- we are going to join the data cid from erp_px to the cst_key. So we are joining data with another table
-/* Avoiding using inner join becasue if other table doesnt have all customer data then we might loose customers.
- Always start with master table and try to avoid inner joins when u r joining it with data from other table */
-
-
- -- Giving user-friendly names and also changing the sequence of the columns
- IF OBJECT_ID('gold.dim_customers', 'V') IS NOT NULL
+-- CREATING THE FIRST VIEW
+IF OBJECT_ID('gold.dim_customers', 'V') IS NOT NULL
 	DROP VIEW gold.dim_customers;
 go
 
@@ -42,3 +36,50 @@ LEFT JOIN silver.erp_cust_az12 ca
 ON ci.cst_key = ca.cid
 LEFT JOIN silver.erp_loc_a101 la
 ON ci.cst_key = la.cid
+
+
+/*===============================================================================================
+Creating the Second View: Dim_products */
+IF OBJECT_ID('gold.dim_products', 'V') IS NOT NULL
+	DROP VIEW gold.dim_products;
+go
+CREATE VIEW gold.dim_products AS
+SELECT 
+	ROW_NUMBER() OVER (ORDER BY pn.prd_start_dt, pn.prd_key) AS product_key,
+	pn.prd_id AS product_id,
+	pn.prd_key AS product_number,
+	pn.prd_nm AS product_name,
+	pn.cat_id AS category_id,
+	pc.cat AS category,
+	pc.subcat AS subcategory,
+	pc.maintainance,
+	pn.prd_cost AS cost,
+	pn.prd_line AS product_line,
+	pn.prd_start_dt AS start_date 
+FROM silver.crm_prd_info pn
+LEFT JOIN silver.erp_px_cat_g1v2 pc
+ON pn.cat_id = pc.id
+WHERE prd_end_dt IS NULL
+
+/*===============================================================================================
+Creating the Third View: fact_sales */
+IF OBJECT_ID('gold.fact_sales', 'V') IS NOT NULL
+	DROP VIEW gold.dim_products;
+go
+	
+CREATE VIEW gold.fact_sales AS
+SELECT 
+sd.sls_ord_num AS order_number,
+pr.product_key,  
+cu.customer_key,  
+sd.sls_order_dt AS order_date,
+sd.sls_ship_dt AS shipping_date,
+sd.sls_due_dt AS due_date,
+sd.sls_sales AS sales_amount,
+sls_quantity AS quantity,
+sls_price AS price
+FROM silver.crm_sales_details sd 
+LEFT JOIN gold.dim_products pr
+ON sd.sls_prd_key = pr.product_number
+LEFT JOIN gold.dim_customers cu
+ON sd.sls_cust_id = cu.customer_id
